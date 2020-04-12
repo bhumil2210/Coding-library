0060885c3f14a7ae9bed0cd416742485752266e2
Bhumil Anil Jakheliya <31176946+bhumil2210@users.noreply.github.com>
2020-04-12 22:51:48 +0530

    Create Basic cpp template.cpp

---
+++ Basic cpp template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define IOFAST ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+
+#define pb push_back  
+#define f first
+#define s second
+
+
+int main(){
+    cout<<"Hello\n";
+    return 1;
+
+}
