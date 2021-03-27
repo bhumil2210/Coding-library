f66695c056bd3e62c8679cb32d61b961c89eda46
Bhumil Anil Jakheliya <31176946+bhumil2210@users.noreply.github.com>
2021-03-27 12:55:02 +0530

    added ordered_set to basic cpp template

--- Basic cpp template.cpp
+++ Basic cpp template.cpp
@@ -1,11 +1,18 @@
 #include<bits/stdc++.h>
-#define IOFAST ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ordered_set tree<pair<long long int,long long int>, null_type,less< pair<long long int,long long int> >, rb_tree_tag,tree_order_statistics_node_update>
+#define IOFAST ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
 #define ll long long
 #define fo(i,n) for(int i=0;i<n;i++)
 
 #define pb push_back  
-
+#define mp make_pair
 #define f first
 #define s second
 
